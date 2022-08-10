6a7040abb80419a894cf814103c818d5ab7c02c3
kikkd <whddls6666@naver.com>
2022-08-10 14:36:15 +0900

    box추가

--- pyqt/2. HI 프로그램/3_radio_check_box.py
+++ pyqt/2. HI 프로그램/3_radio_check_box.py
@@ -11,23 +11,16 @@ class MainWindow(QMainWindow):
         self.ui.btn_hi.clicked.connect(self.click)
 
     def click(self):       
-# ======================================================================================
-# UI버튼 클릭 시 전체 체크박스 자동 클릭
         self.ui.chk_1.setChecked(True)
         self.ui.chk_2.setChecked(True)
         self.ui.chk_3.setChecked(True)
-# ======================================================================================
 
-# ======================================================================================
-# UI버튼 클릭 시 라디오 버큰 순환 클릭
         if self.ui.radio_1.isChecked():
             self.ui.radio_2.setChecked(True)
         elif self.ui.radio_2.isChecked():
             self.ui.radio_1.setChecked(True)
         else:
             self.ui.radio_1.setChecked(True)
-# ======================================================================================
-
 
 if __name__ == "__main__":
     app = QApplication()
