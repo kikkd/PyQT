bcb40d13802b543e8b4d5ff9551399a093546627
kikkd <whddls6666@naver.com>
2022-08-12 09:50:44 +0900

    메신저

---
+++ pyqt/3.메신저 프로그램/1.py
@@ -0,0 +1,32 @@
+from PySide6.QtWidgets import QMainWindow, QApplication, QMessageBox
+from PySide6.QtGui import QStandardItemModel, QStandardItem
+from ui import Ui_MainWindow
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.model = QStandardItemModel()
+        self.ui.list_chat.setModel(self.model)
+        
+        self.ui.btn_send.clicked.connect(self.send)
+    
+    def send (self):
+        # mb = QMessageBox()
+        # mb.setText(self.ui.edit_text.text())
+        # mb.exec()
+        text = self.ui.edit_text.text()
+        item = QStandardItem(text)
+        self.model.appendRow(item)
+
+
+if __name__ == "__main__":
+    app = QApplication()
+
+    window = MainWindow()
+    window.show()
+    
+    sys.exit(app.exec())
