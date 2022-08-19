ca7a8eacf898a30df0ddf120a7a4ca0a7db47af6
kikkd <whddls6666@naver.com>
2022-08-19 14:00:35 +0900

    mail viewer

---
+++ pyqt/5. mail viewer/1_selenium_setting.py
@@ -0,0 +1,45 @@
+from socket import timeout
+from xml.sax.xmlreader import Locator
+from PySide6.QtWidgets import QMainWindow, QApplication, QMessageBox, QTableWidgetItem
+from PySide6.QtGui import QStandardItemModel, QStandardItem
+from PySide6.QtCore import QTimer
+from ui import Ui_MainWindow
+import datetime
+import sys
+
+# pip install selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+# pip install pyperclip
+import pyperclip
+
+def find(chrome,css):
+    wait = WebDriverWait(chrome, 5)
+    return wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,css)))
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+    # seleniu up
+    chrome = webdriver.Chrome("./chromedriver.exe")
+    # log in
+    chrome.get("https://mail.naver.com")
+    input_id = find(chrome,"#id")
+    print(input_id)
+
+
+if __name__ == "__main__":
+    app = QApplication()
+
+    window = MainWindow()
+    window.show()
+    
+    sys.exit(app.exec())
+
---
+++ pyqt/5. mail viewer/ui.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ui.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QLabel, QMainWindow,
+    QSizePolicy, QTableWidget, QTableWidgetItem, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(963, 405)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.tableWidget = QTableWidget(self.centralwidget)
+        if (self.tableWidget.columnCount() < 5):
+            self.tableWidget.setColumnCount(5)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.tableWidget.setGeometry(QRect(2, 2, 701, 400))
+        self.lb_title = QLabel(self.centralwidget)
+        self.lb_title.setObjectName(u"lb_title")
+        self.lb_title.setGeometry(QRect(710, 10, 48, 16))
+        self.lb_content = QLabel(self.centralwidget)
+        self.lb_content.setObjectName(u"lb_content")
+        self.lb_content.setGeometry(QRect(710, 30, 251, 371))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"\ub0a0\uc9dc", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"\uc0c8 \ud589", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"\uc0ac\uc774\ud2b8", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"\ubcf4\ub0b8\uc774", None));
+        ___qtablewidgetitem4 = self.tableWidget.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWindow", u"\uc81c\ubaa9", None));
+        self.lb_title.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.lb_content.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+    # retranslateUi
+
